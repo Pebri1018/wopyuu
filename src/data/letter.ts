22ffb17004edf40bca3133258554c55d02292486
Pebri1018 <pebripratama1018@gmail.com>
2026-06-17 18:24:29 +0700

    update letter

--- src/data/letter.ts
+++ src/data/letter.ts
@@ -1,9 +1,8 @@
 export const letterContent = [
-  "Hai sayang...",
-  "Selamat ulang tahun ya!",
-  "Aku cuma mau bilang...",
-  "Terima kasih sudah jadi pasangan yang luar biasa.",
-  "Aku bersyukur banget bisa kenal dan sama kamu.",
-  "Semoga semua impian kamu tercapai.",
+  "happy birthday sayanggku🥳",
+  "selamat anda sudah kepala duaa woww. aku mau ngucapin selamat ulang tahun sayanggku tercinta, sehat selalu ya kamuu, semoga rezekinya selalu dilancarkan, urusannya selalu di lancarkan, segalanya selalu di lancarkan, dan semua impian kamu segera tercapai, amiinn\n",
+  "aku senang sekali sampe sekarang ini masih bisa temenin kamu meskipun dengan lika liku nya itu yakk wkwk, but aku sangat sangat happy bisa bareng terus sama kamu, saling belajar bareng, bertumbuh bareng, pokoknya bangga deh bisa punya kamu💐💐💐\n",
+  "harapan aku adalah selalu sama kamu atau kalo lagu yang sering aku dengerin tuh liriknya begini 'As long as im with you, I've got a smile on my face' wkwk. pokoknya harapan aku, kita sama sama tumbuh bareng apapun badainya itu, soalnya kalo sama kamu jujurly enteng sih hahaha.\n",
+  "pokoknya aku mau mengucapkan happy birthday sayangg, mwahh😽\n",
   "I love you! ❤️"
 ];
